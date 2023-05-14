6870ccfab877609db3eb773c53afae14f670faa2
ko2ja <ko2ja@DESKTOP-4P6SQK2>
2023-05-14 17:31:17 +0200

    Tercer commit 3

--- Repaso_Mayo_14/src/Repaso_14_Mayo.java
+++ Repaso_Mayo_14/src/Repaso_14_Mayo.java
@@ -5,6 +5,8 @@ public class Repaso_14_Mayo {
 		
 		System.out.println("Tests de 14 mayo del 2023");
 		System.out.println("Tests segunda parte!!");
+		
+		System.out.println("Tests tercera parte!!");
 	}
 
 }
