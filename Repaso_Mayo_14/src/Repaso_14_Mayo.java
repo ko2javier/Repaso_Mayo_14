cf0a960e26fa215a548d8e0fa1cd85ff5ea44755
ko2ja <ko2ja@DESKTOP-4P6SQK2>
2023-05-14 17:29:37 +0200

    probamos commit 2

--- Repaso_Mayo_14/src/Repaso_14_Mayo.java
+++ Repaso_Mayo_14/src/Repaso_14_Mayo.java
@@ -4,6 +4,7 @@ public class Repaso_14_Mayo {
 	public static void main(String[] args) {
 		
 		System.out.println("Tests de 14 mayo del 2023");
+		System.out.println("Tests segunda parte!!");
 	}
 
 }
